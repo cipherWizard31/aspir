b905e61f554c9908902eafac830b265c9b7a9963
cipherWizard31 <nathanyidnekachew383@gmail.com>
2026-02-22 02:10:04 +0300

    Update Home component layout with buttons and images for Bucket List and Wishlist

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,27 @@
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <div className="flex h-screen items-center justify-center space-x-52">
+      <button className="p-15 duration-300 rounded-3xl hover:-translate-y-4 hover:shadow-xl">
+        <Image
+          src="/water-bucket.png"
+          height={200}
+          width={200}
+          alt="water bucket"
+        />
+        <h1 className="pt-6 text-xl">Bucket List</h1>
+      </button>
+      {/* <div className="p-15 duration-300 rounded-3xl hover:-translate-y-4 hover:shadow-xl"> */}
+      <div>
+        <Image
+          src="/spring-notebook.png"
+          height={200}
+          width={200}
+          alt="spring notebook"
+          />
+        <h1 className="pt-6 text-xl">Wishlist</h1>
+      </div>
     </div>
   );
 }
