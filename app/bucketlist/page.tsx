e73facee6f338b0a07005d5eec6f844dd320dbd1
cipherWizard31 <nathanyidnekachew383@gmail.com>
2026-03-06 23:02:01 +0300

    Add Wishlist page and fetch items from API

---
+++ app/bucketlist/page.tsx
@@ -0,0 +1,23 @@
+async function getWishlist() {
+    const res = await fetch("http://localhost:3000/api/bucketlist", {
+        cache: "no-store"
+      })
+    return res.json()
+  }
+  
+  export default async function WishlistPage() {
+    const items = await getWishlist()
+  
+    return (
+      <div>
+        <h1>My Wishlist</h1>
+        <ul>
+          {items.map((item: any, index: number) => (
+            <li key={index}>
+              {item.title}
+            </li>
+          ))}
+        </ul>
+      </div>
+    )
+  }
