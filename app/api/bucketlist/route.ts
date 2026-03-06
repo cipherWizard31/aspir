b60781b8bc93286b833a8bbc2ba1c0731787a7b5
cipherWizard31 <nathanyidnekachew383@gmail.com>
2026-03-06 22:59:45 +0300

    Refactor wishlist and bucket list API routes: update item addition response messages and clean up code

--- app/api/bucketlist/route.ts
+++ app/api/bucketlist/route.ts
@@ -1,30 +1,24 @@
-import { NextResponse } from "next/server";
-import {promises as fs} from "fs";
-import path from "path";
-import { RequestStore } from "next/dist/server/app-render/work-unit-async-storage.external";
+import { NextResponse } from "next/server"
+import { promises as fs } from "fs"
+import path from "path"
 
+const filePath = path.join(process.cwd(), "data", "bucketlist.json")
 
-
-const filePath = path.join(process.cwd(), "data", "items.json")
-
-
-// GET Items
 export async function GET() {
-    const data = await fs.readFile(filePath, "utf-8")
-    const items = JSON.parse(data)
-    return NextResponse.json(items)
+  const data = await fs.readFile(filePath, "utf-8")
+  const items = JSON.parse(data)
+  return NextResponse.json(items)
 }
 
-//POST new item
 export async function POST(req: Request) {
-    const newItem = await req.json()
+  const newItem = await req.json()
 
-    const data = await fs.readFile(filePath, "utf-8")
-    const items = JSON.parse(data)
-    
-    
-    items.push(newItem)
-    await fs.writeFile(filePath, JSON.stringify(items, null, 2))
+  const data = await fs.readFile(filePath, "utf-8")
+  const items = JSON.parse(data)
 
-    return NextResponse.json({message: "Item added"})
+  items.push(newItem)
+
+  await fs.writeFile(filePath, JSON.stringify(items, null, 2))
+
+  return NextResponse.json({ message: "Wishlist item added" })
 }
